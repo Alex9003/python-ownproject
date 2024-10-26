f0eca8e049013d9e151b9c9f41c2fe19b8f365a6
Alex_9003 <agl.luchko@gmail.com>
2024-10-26 14:14:25 +0300

    second

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 from flask import Flask, request, session, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+from sqlalchemy import false
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'car_shop.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 # === MODERLS =================
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(25), nullable=False)
+    role = db.Column(db.Integer(), nullable=False, default=False)
 
+    def __repr__(self):
+        return f'<User: {self.username}>'
 
 # === ROUTES ==================
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    # код .....
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/car')
+def car():
+    return render_template('car.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__=='__main__':
     app.run(debug=True)
